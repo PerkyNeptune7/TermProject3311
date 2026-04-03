51f507f59a4c0060103c96ff776eef154dda2322
PerkyNeptune7 <shitijaggarwal07@gmail.com>
2026-04-02 21:04:10 -0400

    adding sama's files

---
+++ src/test/randoop/NewFeaturesRegressionTest.java
@@ -0,0 +1,9 @@
+package src.test.randoop;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ NewFeaturesRegressionTest0.class, NewFeaturesRegressionTest1.class, NewFeaturesRegressionTest2.class, NewFeaturesRegressionTest3.class, NewFeaturesRegressionTest4.class, NewFeaturesRegressionTest5.class, NewFeaturesRegressionTest6.class, NewFeaturesRegressionTest7.class, NewFeaturesRegressionTest8.class })
+public class NewFeaturesRegressionTest {
+}
+
